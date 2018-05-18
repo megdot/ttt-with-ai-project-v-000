0e615c5b48df2865dd311dd31e83f9b4cd9d92a4
Megan Dotto <megan.dotto@gmail.com>
2018-05-18 05:38:44 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -29,6 +29,7 @@ class Game
     WIN_COMBINATIONS.detect do |winner|
       @board.cells[winner[0]] == @board.cells[winner[1]] &&
       @board.cells[winner[1]] == @board.cells[winner[2]]
+      @board.cells[winner[0]] == "X" || "O"
     end
   end
 
