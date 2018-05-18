1a141d13664551754ee73566daa9e689271216c8
Megan Dotto <megan.dotto@gmail.com>
2018-05-18 05:22:32 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -28,13 +28,10 @@ class Game
   def won?
     WIN_COMBINATIONS.detect do |winner|
       @board.cells[winner[0]] == @board.cells[winner[1]] &&
-      @board.cells[winner[1]] == @board.cells[winner[2]] 
+      @board.cells[winner[1]] == @board.cells[winner[2]]
     end
   end
 
-    end
-  end
 
-  end
 
 end
